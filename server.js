796cc25eb34bbb8379711533000b563c7b3d1b43
htmlrulit <34247867+htmlrulit@users.noreply.github.com>
2024-04-19 19:14:54 +0300

    Фикс багов
    Там всякие лимиты, то сё

--- server.js
+++ server.js
@@ -12,10 +12,19 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const md5 = require('md5');
 const clientSecret = '';
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+    host: 'localhost',
+    port: 6379
+});
+
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.connect();
 
 const client = new Client({
     user: '',
-    host: '',
+    host: 'localhost',
     database: '',
     password: '',
     port: 5432,
@@ -37,6 +46,38 @@ const generateSignature = (params) => {
     const paramsString = `user_id=${user_id}&test_id=${test_id}&score=${score}${clientSecret}`;
     return md5(paramsString);
 };
+
+const updateTopUsersCache = async () => {
+    const cacheKey = 'topusers';
+    try {
+        const topUsersQuery = `
+            SELECT
+                user_id,
+                NULL AS first_name,
+                NULL AS last_name,
+                NULL AS avatar,
+                SUM(score) AS score
+            FROM
+                test_results
+            GROUP BY
+                user_id
+            ORDER BY
+                score DESC
+            LIMIT
+                10;
+        `;
+        const topUsersResult = await client.query(topUsersQuery);
+        const topUsers = topUsersResult.rows;
+
+        await redisClient.setEx(cacheKey, 60, JSON.stringify(topUsers));  // Установка кэша на 60 секунд для надежности
+
+        console.log("Top users cache updated successfully:", new Date());
+    } catch (error) {
+        console.error('Error updating top users cache:', error);
+    }
+};
+
+setInterval(updateTopUsersCache, 15000);
 
 const verifyLaunchParams = (params, receivedSignature) => {
     const expectedSignature = generateSignature(params);
@@ -101,39 +142,38 @@ app.get('/tests', async (req, res) => {
 });
 
 app.get('/api/topusers', async (req, res) => {
+    const cacheKey = 'topusers';
+
     try {
-        console.log('Received a GET request to fetch top users');
+        console.log("Attempting to retrieve top users from cache...");
+        const cachedTopUsers = await redisClient.get(cacheKey);
 
-        const topUsersQuery = `
-            SELECT
-                user_id,
-                NULL AS first_name,
-                NULL AS last_name,
-                NULL AS avatar,
-                SUM(score) AS score
-            FROM
-                test_results
-            GROUP BY
-                user_id
-            ORDER BY
-                score DESC
-            LIMIT
-                10;
-        `;
-
-        const topUsersResult = await client.query(topUsersQuery);
-        const topUsers = topUsersResult.rows;
-
-        res.status(200).json(topUsers);
+        if (cachedTopUsers) {
+            console.log("Top users data retrieved from cache.");
+            res.status(200).json(JSON.parse(cachedTopUsers));
+        } else {
+            console.log("No cached top users data available.");
+            res.status(500).json({ error: 'Cache miss, please try again' });
+        }
     } catch (error) {
-        console.error('Error fetching top users:', error);
+        console.error('Error fetching top users from cache:', error);
         res.status(500).json({ error: 'An error occurred while fetching top users data' });
     }
 });
 
 app.get('/leaderboard', async (req, res) => {
+    const cacheKey = 'leaderboard';
+
     try {
-        console.log('Received a GET request to fetch leaderboard');
+        console.log("Attempting to retrieve leaderboard from cache...");
+        const cachedLeaderboard = await redisClient.get(cacheKey);
+
+        if (cachedLeaderboard) {
+            console.log("Leaderboard data retrieved from cache.");
+            return res.status(200).json(JSON.parse(cachedLeaderboard));
+        }
+
+        console.log("No cached leaderboard found, querying database...");
         const leaderboardQuery = `
             SELECT user_id, users.first_name, users.last_name, leaderboard.score
             FROM test_results
@@ -143,6 +183,13 @@ app.get('/leaderboard', async (req, res) => {
         `;
         const leaderboardResult = await client.query(leaderboardQuery);
         const leaderboard = leaderboardResult.rows;
+
+        console.log("Leaderboard data retrieved from database: ", leaderboard);
+
+        console.log("Caching new leaderboard data...");
+        await redisClient.setEx(cacheKey, 15, JSON.stringify(leaderboard));
+
+        console.log("New leaderboard data cached.");
         res.status(200).json(leaderboard);
     } catch (error) {
         console.error('Error fetching leaderboard:', error);
@@ -150,59 +197,58 @@ app.get('/leaderboard', async (req, res) => {
     }
 });
 
+
 app.post('/save-test-result', async (req, res) => {
+    const { vk_user_id, score, test_id, user_id, totalQuestions } = req.body;
+    const receivedSignature = req.headers['x-signature'];
+    const lockKey = `lock:testResult:${user_id}:${test_id}`;
+
     try {
-        console.log("Received data:", req.body);
+        const lockSet = await redisClient.set(lockKey, 'locked', 'NX', 'PX', 30000);
+        if (!lockSet) {
+            console.log('server: Operation is currently in progress for user:', user_id, 'test:', test_id);
+            return res.status(429).json({ error: 'Operation is currently in progress.' });
+        }
 
-        const {
-            vk_access_token_settings, vk_app_id, vk_are_notifications_enabled,
-            vk_is_app_user, vk_is_favorite, vk_language, vk_platform, vk_ref,
-            vk_ts, vk_user_id, timestamp, nonce, score, test_id, user_id, totalQuestions
-        } = req.body;
+        if (!verifyLaunchParams(req.body, receivedSignature)) {
+            console.log('server: Invalid signature detected.');
+            return res.status(400).json({ error: 'Invalid signature' });
+        }
 
-        const params = {
-            vk_access_token_settings, vk_app_id, vk_are_notifications_enabled,
-            vk_is_app_user, vk_is_favorite, vk_language, vk_platform, vk_ref,
-            vk_ts, vk_user_id, timestamp, nonce, score, test_id, user_id, totalQuestions
-        };
-
-        const receivedSignature = req.headers['x-signature'];
-        const signatureVerificationResult = await verifyLaunchParams(params, receivedSignature);
-        if (!signatureVerificationResult) {
-            res.status(400).json({ error: 'Invalid signature' });
-            return;
-        }
-        if (user_id !== vk_user_id) {
-            console.log('server: User ID does not match vk_user_id');
-            return res.status(400).json({ error: 'User ID mismatch' });
-        }
-        if (typeof user_id !== 'number' || typeof test_id !== 'number' || typeof score !== 'number' || typeof totalQuestions !== 'number') {
-            return res.status(400).json({ error: 'Invalid data format' });
-        }
-        if (score < 0 || score > totalQuestions) {
-            return res.status(400).json({ error: 'Invalid score value' });
-        }
-        const resultQuery = `SELECT * FROM test_results WHERE user_id = $1 AND test_id = $2`;
-        const resultValues = [user_id, test_id];
-        const result = await client.query(resultQuery, resultValues);
+        const now = new Date();
+        const resultQuery = `SELECT timestamp FROM test_results WHERE user_id = $1 AND test_id = $2 ORDER BY timestamp DESC LIMIT 1`;
+        const result = await client.query(resultQuery, [user_id, test_id]);
         const existingResult = result.rows[0];
 
         if (existingResult) {
-            const updateQuery = `UPDATE test_results SET score = $1, timestamp = NOW() WHERE user_id = $2 AND test_id = $3`;
-            await client.query(updateQuery, [score, user_id, test_id]);
-            console.log('server: Test result updated successfully');
-        } else {
-            const insertQuery = `INSERT INTO test_results (user_id, test_id, score, timestamp) VALUES ($1, $2, $3, NOW())`;
-            await client.query(insertQuery, [user_id, test_id, score]);
-            console.log('server: New test result saved successfully');
+            const lastTestTime = new Date(existingResult.timestamp);
+            const timeDiff = (now - lastTestTime) / 1000;
+            console.log(`Time difference since last test: ${timeDiff} seconds`);
+            if (timeDiff < 15) {
+                console.log(`Submission too soon. Time difference: ${timeDiff} seconds.`);
+                return res.status(429).json({ error: 'Submission too soon. Please wait longer.' });
+            }
         }
+
+        const updateOrInsertQuery = existingResult ?
+            `UPDATE test_results SET score = ${score}, timestamp = NOW() WHERE user_id = ${user_id} AND test_id = ${test_id}` :
+            `INSERT INTO test_results (user_id, test_id, score, timestamp) VALUES (${user_id}, ${test_id}, ${score}, NOW())`;
+
+        console.log(`Executing query: ${updateOrInsertQuery}`);
+
+        await client.query(updateOrInsertQuery);
+        console.log(`Test result for user ${user_id} on test ${test_id} saved or updated successfully.`);
+
         res.status(200).json({ success: true });
     } catch (error) {
-        console.error('Error saving test result:', error);
-        logErrorToFile(error);
+        console.error('Error:', error);
         res.status(500).json({ error: 'An error occurred while saving test result' });
+    } finally {
+        await redisClient.del(lockKey);
     }
 });
+
+
 
 const httpsOptions = {
     key: fs.readFileSync('key.key'),
